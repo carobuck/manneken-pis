097ef92aa413d64e556786e6b52bfbd705edeb07
Caroline Buck <carobuck@Carolines-MacBook-Air.local>
2024-07-22 17:14:12 -0400

    final commit

--- get-json-costumes.R
+++ get-json-costumes.R
@@ -40,5 +40,38 @@ combined_df_costume %>%
   full_join(ref_theme,by=join_by(id_theme==id)) %>%
   mutate(t = str_trim(t),
          body = str_trim(body)) %>%
-  distinct() %>%
-  glimpse()
+  distinct() -> clean_data
+
+
+## Let's plot a dumb lil chart
+library(treemapify)
+clean_data %>%
+  #count(geo,theme) %>%
+  count(theme) %>%
+  filter(!is.na(theme)) %>%
+  ggplot(aes(y=n,x=fct_rev(fct_reorder(theme,n)))) +
+  geom_col(fill='cyan4',color='gray30') +
+  geom_label(aes(label=n),hjust = -0.1) +
+  coord_flip() +
+  theme_bw() +
+  labs(y='Number of Costumes',x='',title='Costumes by Themed Category') +
+  theme(axis.text = element_text(size=11)) 
+
+
+clean_data %>%
+  #count(geo,theme) %>%
+  count(geo) %>%
+  filter(n>=10) %>%
+  ggplot(aes(area = n, fill = geo, label = paste(n,geo))) +
+  geom_treemap(color='white') +
+  geom_treemap_text(colour = "black",
+                    place = "centre",
+                    size = 15) +
+  theme(legend.position = 'none') +
+  labs(title='Locations with at least 10 Outfits')
+
+
+# get stats for total number of locations and # themes
+
+clean_data %>%
+  count(theme)
